a01bb68ae29b961e4c36561bf7ab9e6561975444
Vlad Buzea <vlad.buzea@itiviti.com>
2019-04-18 13:06:54 +0300

    Add Scopes
    Also added proxy
    
    Change-Id: I773bfdf6db4604fa22179e90b8673bb401420252

--- react-oauth/server.js
+++ react-oauth/server.js
@@ -2,6 +2,13 @@ const express = require("express");
 require("dotenv").config();
 const jwt = require("express-jwt"); // Validate JWT and set req.user
 const jwksRsa = require("jwks-rsa"); // Retrieve RSA keys from a JSON Web Key set (JWKS) endpoint
+const checkScope = require("express-jwt-authz");
+
+const globalTunnel = require('global-tunnel-ng');
+globalTunnel.initialize({
+    host: 'proxy-cluj',
+    port: 9876
+});
 
 const checkJwt = jwt({
     // Dynamically provide a signing key based on the kid in the header
@@ -27,15 +34,41 @@ const app = express();
 
 app.enable('trust proxy');
 
-app.get("/public", function(req, res) {
+app.get("/public", function (req, res) {
     res.json({
         message: "Hello from a public API!"
     });
 });
 
-app.get("/private", checkJwt, function(req, res) {
+app.get("/private", checkJwt, function (req, res) {
     res.json({
         message: "Hello from a private API!"
+    });
+});
+
+app.get("/courses", checkJwt, checkScope(["read:courses"]), function (req, res) {
+    res.json({
+        courses: [
+            {id: 1, title: "Vlad + React"},
+            {id: 2, title: "Vlad + OAuth"}
+        ]
+    });
+});
+
+function checkRole(role) {
+    return function (req, res, next) {
+        const assignedRoles = req.user["http://localhost:3000/roles"];
+        if (Array.isArray(assignedRoles) && assignedRoles.includes(role)) {
+            return next();
+        } else {
+            return res.status(401).send("Insufficient role");
+        }
+    };
+}
+
+app.get("/admin", checkJwt, checkRole('admin'), function (req, res) {
+    res.json({
+        message: "Hello from a ADMIN API!"
     });
 });
 
