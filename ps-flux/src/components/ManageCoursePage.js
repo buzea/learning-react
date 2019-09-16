39a77dd3250a4bb937f647dea808a62c8e8f9477
Vlad Buzea <vlad.buzea@itiviti.com>
2019-09-16 16:07:44 +0300

    ps-flux Form with validation
    Change-Id: I1a3b42836926da8fd3d160e723e4ad9a4426d190

--- ps-flux/src/components/ManageCoursePage.js
+++ ps-flux/src/components/ManageCoursePage.js
@@ -1,11 +1,11 @@
-import React, {useState} from 'react';
-import {Prompt} from "react-router-dom";
+import React, {useState, useEffect} from 'react';
 import CourseForm from "./CourseForm";
 import * as courseApi from "../api/courseApi";
 import {toast} from "react-toastify";
 
 const ManageCoursePage = props => {
 
+    const [errors, setErrors] = useState({});
     const [course, setCourse] = useState({
         id: null,
         slug: "",
@@ -14,13 +14,34 @@ const ManageCoursePage = props => {
         category: ""
     });
 
+    useEffect(() => {
+        const slug = props.match.params.slug;
+        if(slug) {
+            courseApi.getCourseBySlug(slug)
+                .then(_course => setCourse(_course));
+        }
+    }, [props.match.params.slug]);
+
     const handleChange = ({target}) => {
         const updatedCourse = {...course, [target.name]: target.value};
         setCourse(updatedCourse);
     };
 
+    function formIsValid() {
+        const _errors = {};
+
+        if (!course.title) _errors.title = "Title is required";
+        if (!course.authorId) _errors.authorId = "Author is required";
+        if (!course.category) _errors.category = "Category is required";
+
+        setErrors(_errors);
+
+        return Object.keys(_errors).length === 0;
+    }
+
     function handleSubmit(event) {
         event.preventDefault();
+        if (!formIsValid()) return;
         courseApi.saveCourse(course)
             .then(() => {
                 toast.success("Course saved");
@@ -33,7 +54,11 @@ const ManageCoursePage = props => {
             <h2>Manage Course</h2>
             {/*<Prompt when={true} message={"Are you sure you want to leave?"}/>*/}
             {/*{props.match.params.slug}*/}
-            <CourseForm course={course} onChange={handleChange} onSubmit={handleSubmit}/>
+            <CourseForm
+                errors={errors}
+                course={course}
+                onChange={handleChange}
+                onSubmit={handleSubmit}/>
         </>
     );
 };
